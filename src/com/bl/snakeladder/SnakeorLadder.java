fdfe206423c28c03e848d4daa3fe5309ebcb052d
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-13 16:23:30 +0530

    Added SnakeorLadder programme in Repository

---
+++ src/com/bl/snakeladder/SnakeorLadder.java
@@ -0,0 +1,11 @@
+package com.bl.snakeladder;
+
+public class SnakeorLadder {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Snake & Ladder Programme");
+
+
+
+    }
+}
