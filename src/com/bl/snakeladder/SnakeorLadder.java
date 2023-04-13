11924b57bfceae1b2a7aca656b9def03216bc743
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-13 17:10:51 +0530

    Added Dice code to the SnakeLadder

--- src/com/bl/snakeladder/SnakeorLadder.java
+++ src/com/bl/snakeladder/SnakeorLadder.java
@@ -1,11 +1,32 @@
 package com.bl.snakeladder;
 
+import java.util.Scanner;
+
 public class SnakeorLadder {
     public static void main(String[] args) {
 
-        System.out.println("Welcome To Snake & Ladder Programme");
+        System.out.println("Welcome To Snake & Ladder Simulator");
+
+        //Starting Positions;
+
+        int position = 0;
+
+        int[][] SnakeLadder = {{15, 22}, {14, 7}, {77, 32}, {95, 10}, {50, 32}};
+
+        while (position < 100){
+
+            Scanner ab = new Scanner(System.in);
+            System.out.println("Press Enter for Roll the Dice");
+            ab.nextLine();
 
 
+            int dice = (int)(Math.random() * 6) + 1;
+            System.out.println("You Got: "+ dice);
+
+            position += dice;
+            break;
+
+        }
 
     }
 }
